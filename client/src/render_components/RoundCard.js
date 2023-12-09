a493200db5a23c9439ff85ec81cde2d462808970
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 06:40:36 -0800

    change width

--- client/src/render_components/RoundCard.js
+++ client/src/render_components/RoundCard.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 import logo from '../logo.svg';
 const RoundCard = () => {
     return (
-        <Card style={{width:'220px', height:'220px'}}>
+        <Card style={{width:'220px', height:'220px',}}  className='m-2 bg-dark' border='primary'>
             <Image src={logo} alt='Technology Image' style={{width:'160px', height:'160px'}} roundedCircle/>
             <div style={{width:'220px'}}>
                 <b style={{fontSize:'0.7rem'}}>Programming language</b>
