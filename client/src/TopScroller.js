9ddb7bea70fc926c015f8cf4b2427450478a60ee
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 06:39:33 -0800

    create topscroller

---
+++ client/src/TopScroller.js
@@ -0,0 +1,22 @@
+
+const TopScroller = () => {
+
+    return ( 
+        <a href="#">
+            <div style={{
+                borderRadius: '50%',
+                width: '50px',
+                height: '50px',
+                position: 'fixed',
+                bottom: '100px',
+                backgroundColor: 'blue',
+                textAlign: 'center',
+                color: 'white'
+            }}>
+            <i className="fas fa-up-long"></i>            
+        </div>
+        </a>
+    );
+}
+ 
+export default TopScroller;
