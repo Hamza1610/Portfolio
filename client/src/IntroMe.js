fe41b439f536598b70716c9c97f10f11e31857af
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 02:43:12 -0800

    modify offcanvas color

--- client/src/IntroMe.js
+++ client/src/IntroMe.js
@@ -1,19 +1,22 @@
-import './IntroMe.css'
-import my_picture from './resources/me.jpg';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import logo from './resources/me7.png'
 const IntroMe = () => {
     return ( 
-        <div className="brief_self">
-            <div className="self_info">
-                <div className="who_question">Hamza Muhammad</div>
-                <div className="brief_me">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae iste enim sapiente voluptates rerum quae maxime id iusto reiciendis odio similique eaque, culpa sequi. Id commodi est, similique suscipit pariatur dicta perspiciatis. Autem reprehenderit id ex, sed animi nihil sit quasi quis consequuntur cupiditate aliquam libero, excepturi, assumenda accusamus!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi beatae iste enim sapiente voluptates rerum quae maxime id iusto reiciendis odio similique eaque, culpa sequi. Id commodi est, similique suscipit pariatur dicta perspiciatis. Autem reprehenderit id ex, sed animi nihil sit quasi quis consequuntur cupiditate aliquam libero, excepturi, assumenda accusamus!</p>
+        <Container id='top' className='w-95 h-100' style={{display: 'flex', flexWrap: 'wrap',justifyContent: 'space-around'}}>
+                <div>
+                    <Image src={logo} style={{width: '300px', height: '350px'}}/>  
                 </div>
-            </div>
-            <div className="self_pic_div">
-                <img className="pic" src={ my_picture } alt="Me" />
-            </div>
-        </div>
+                <div style={{width: '550px'}}>
+                    <h1>Software Engineer</h1> 
+                    <div>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt distinctio excepturi nulla quod, atque cumque quo tempore corporis veritatis consectetur hic rerum? Reiciendis vero dolorum nulla molestias non, debitis qui iusto quibusdam rem id ratione ipsum, exercitationem omnis odit repellat. Similique totam aperiam exercitationem ut laudantium accusamus minima soluta commodi.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis quas placeat suscipit quaerat amet facilis iure iste excepturi consequuntur praesentium, deserunt ipsam voluptas.</p>
+                    </div>
+                </div>
+        </Container>
     );
 }
  
