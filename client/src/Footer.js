60d1fbe9ba7c697a2b7110a3a44a981d3d2e1e25
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 06:37:53 -0800

    modify scroller

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,15 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
+import TopScroller from './TopScroller'
 const Footer = () => {
 
     return ( 
         <footer className="bg-dark text-light mt-5">
             <Container>
+                <Row style={{float: 'inline-end'}}>
+                    <TopScroller />
+                </Row>
                 <Row>
-                    <Col className="text-center py-3">
-                        <p>&copy; {new Date().getFullYear()} Muuhammad Hamza. All rights reserved.</p>
+                    <Col className="text-center py-3 bg-dark">
+                        <p className='bg-dark text-center'>&copy; {new Date().getFullYear()} Muuhammad Hamza. All rights reserved.</p>
                     </Col>
                 </Row>
             </Container>
