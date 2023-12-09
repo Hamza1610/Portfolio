9b25fd0f30c9b91d7b13936566d6527210f9d276
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 02:42:15 -0800

    add footer style

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,9 +1,19 @@
-import './Footer.css';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 const Footer = () => {
+
     return ( 
-        <footer className="footer">
-            <p>@MHN Company Limited</p>
-        </footer>
+        <footer className="bg-dark text-light mt-5">
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        <p>&copy; {new Date().getFullYear()} Muuhammad Hamza. All rights reserved.</p>
+                    </Col>
+                </Row>
+            </Container>
+    </footer>
      );
 }
  
