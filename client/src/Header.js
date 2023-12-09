ceb8bdc98213386cab27989d3884d47f5f9f75b8
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 02:42:42 -0800

    add nav items

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,19 +1,39 @@
-import './Header.css';
-const Header = () => {
-  const click = () => console.log("Button clicked");
-    return (
-      <header className="App-header">
-        <div className='logo'>
-          {/* <img className='p-logo' src=''/> to be here*/}
-          <button className="btn-mode" onclick = { click }>Dark Mode</button>
-        </div>
-        <div className='menu_container'>
-          <a href='#aboutP'><div className='about_me'> About me</div></a>
-          <a href='#projectP'><div className='my_projects'>Projects</div></a>
-          <a href='#contactP'><div className='contact_me' >Contact me</div></a>
-        </div>
-    </header>
-    );
-}
- 
-export default Header;
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from "react-bootstrap/Nav";
+import Container from 'react-bootstrap/Container';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useState } from 'react';
+
+const Navigation = () => {
+
+  const [offcanvasStyle, setOffcanvasStyle] = useState({color: 'black'})
+
+  return (
+    <Navbar expand="md">
+      <Container className='justify-content-between'>
+        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+        <Navbar.Offcanvas
+          id={`offcanvasNavbarLabel-expand-lg`}
+          aria-labelled-by={`offcanvasNavbarLabel-expand-lg`}
+          placement='end'
+          onEntering={()=> setOffcanvasStyle({color: 'black'})}
+          onExiting={()=> setOffcanvasStyle({color: 'black'})}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body className='justify-content-end'>
+              <Nav fill variant='pills' defaultActiveKey='/'>
+              <Nav.Link href='#projects' className='me-4' style={offcanvasStyle}>Projects</Nav.Link>
+              <Nav.Link href='#experience' className='me-3' style={offcanvasStyle}>Experience</Nav.Link>
+              <Nav.Link href='#technologies' className='me-4' style={offcanvasStyle}>Technologies</Nav.Link>
+              <Nav.Link href='#contact' className='me-4' style={offcanvasStyle}>Contact</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
