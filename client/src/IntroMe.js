96a08671d52e86bfde9d658a04ba7acca0b6ff68
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 06:38:51 -0800

    style intro div

--- client/src/IntroMe.js
+++ client/src/IntroMe.js
@@ -5,15 +5,14 @@ import Container from 'react-bootstrap/Container';
 import logo from './resources/me7.png'
 const IntroMe = () => {
     return ( 
-        <Container id='top' className='w-95 h-100' style={{display: 'flex', flexWrap: 'wrap',justifyContent: 'space-around'}}>
-                <div>
-                    <Image src={logo} style={{width: '300px', height: '350px'}}/>  
+        <Container id='top' className='w-95 h-100 mt-5' style={{display: 'flex', flexWrap: 'wrap',justifyContent: 'space-around'}}>
+                <div style={{display:'flex', }}>
+                    <Image src={logo} style={{width: '400px', height: '450px'}}/>  
                 </div>
-                <div style={{width: '550px'}}>
-                    <h1>Software Engineer</h1> 
+                <div style={{width: '565px'}}>
+                    <h3>Full Stack Software Engineer</h3> 
                     <div>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt distinctio excepturi nulla quod, atque cumque quo tempore corporis veritatis consectetur hic rerum? Reiciendis vero dolorum nulla molestias non, debitis qui iusto quibusdam rem id ratione ipsum, exercitationem omnis odit repellat. Similique totam aperiam exercitationem ut laudantium accusamus minima soluta commodi.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis quas placeat suscipit quaerat amet facilis iure iste excepturi consequuntur praesentium, deserunt ipsam voluptas.</p>
                     </div>
                 </div>
         </Container>
