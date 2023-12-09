4c24b7a5b14011a9147294af57ae3bcd3761e3ad
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 06:38:27 -0800

    change offcanvas color

--- client/src/Header.js
+++ client/src/Header.js
@@ -6,18 +6,18 @@ import { useState } from 'react';
 
 const Navigation = () => {
 
-  const [offcanvasStyle, setOffcanvasStyle] = useState({color: 'black'})
+  const [offcanvasStyle, setOffcanvasStyle] = useState({color: 'white'})
 
   return (
     <Navbar expand="md">
       <Container className='justify-content-between'>
-        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' variant='primary'/>
         <Navbar.Offcanvas
           id={`offcanvasNavbarLabel-expand-lg`}
           aria-labelled-by={`offcanvasNavbarLabel-expand-lg`}
           placement='end'
           onEntering={()=> setOffcanvasStyle({color: 'black'})}
-          onExiting={()=> setOffcanvasStyle({color: 'black'})}
+          onExiting={()=> setOffcanvasStyle({color: 'white'})}
           >
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>Menu</Offcanvas.Title>
