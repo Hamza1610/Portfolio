75712405e9726af5c7a39f2b1312ffe76788ceb8
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 02:43:42 -0800

    modify projects to array

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,77 +1,18 @@
-import './Projects.css';
-import fotoproject from'./resources/contact_facebook.webp';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import CustomCard from './render_components/Card';
+
 const Projects = (props) => {
-    /* Is a list with object items holding data as database */
-    const projectData1 = [
-        {id : 1, projectTitle: 'Hospital Management System', dateCreated: '01/01/1990', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'},
-        {id : 2, projectTitle: 'Real Estate Management System', dateCreated: '02/02/1991', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'},
-    ];
-
-    const projectData2 = [
-        {id : 3, projectTitle: 'School Management System', dateCreated: '03/03/1992', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'},
-        {id : 4, projectTitle: 'Computer Management Stystem', dateCreated: '04/04/1993', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'},
-    ];
-    const projectData3 = [
-        {id : 5, projectTitle: 'Database Management System', dateCreated: '05/05/1994', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'},
-        {id : 6, projectTitle: 'Control Room Project', dateCreated: '06/06/1995', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'},
-    ];
-
-    const projectData4 = [
-        {id : 7, projectTitle: 'Mobile money reciept', dateCreated: '07/07/1996', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'},
-        {id : 8, projectTitle: 'Attendance marker', dateCreated: '08/08/1997', projectInfo: 'Lorem ifuhfud dufhijkn ufzu fdknjcfxui hdfvcxnvjk  uahugdsiufds uifdsu fdhzzjz dsfhydf ivdfu ufidf diuuidsghds ds ujdf dsui...'}
-    ];
-    const alt = props.alt;
+    const projects = [1,2,3,4,5,6,7,8,9]
     return (
-        <div className="project_div" id="project_div">
-            <div className = "set">
-            {projectData1.map((proj) =>(
-                <div className="proj_template">
-                    <div>
-                        <img className="project_img" src={ fotoproject }  alt= { alt } />
-                    </div>
-                    <h1 className="topic_head">{ proj.projectTitle }</h1>
-                    <p className="topic_info">{ proj.projectInfo }</p>
-                    <small className="topic_date">{ proj.dateCreated }</small>
-                </div>
-            ))}
-            </div>
-            <div className = "set">
-            {projectData2.map((proj) =>(
-                <div className="proj_template">
-                    <div>
-                        <img className="project_img" src={ fotoproject }  alt= { alt } />
-                    </div>
-                    <h1 className="topic_head">{ proj.projectTitle }</h1>
-                    <p className="topic_info">{ proj.projectInfo }</p>
-                    <small className="topic_date">{ proj.dateCreated }</small>
-                </div>
-            ))}
-            </div>
-            <div className = "set">
-            {projectData3.map((proj) =>(
-                <div className="proj_template">
-                    <div>
-                        <img className="project_img" src={ fotoproject }  alt= { alt } />
-                    </div>
-                    <h1 className="topic_head">{ proj.projectTitle }</h1>
-                    <p className="topic_info">{ proj.projectInfo }</p>
-                    <small className="topic_date">{ proj.dateCreated }</small>
-                </div>
-            ))}
-            </div>
-            <div className = "set">
-            {projectData4.map((proj) =>(
-                <div className="proj_template">
-                    <div>
-                        <img className="project_img" src={ fotoproject }  alt= { alt } />
-                    </div>
-                    <h1 className="topic_head">{ proj.projectTitle }</h1>
-                    <p className="topic_info">{ proj.projectInfo }</p>
-                    <small className="topic_date">{ proj.dateCreated }</small>
-                </div>
-            ))}
-            </div>
-        </div>
+        <Container>
+            <Row md={1}  lg={3}>
+
+                {/* For now project will be rendered as template later from database */}
+                {projects && projects.map((item) => (<CustomCard content = {item}/>))}
+            </Row>
+        </Container>
     );
 }
  
