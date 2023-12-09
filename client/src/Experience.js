1fc50f0b1a1175fdb38b5d0792d24a0e861ec3e0
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-08 17:26:47 -0800

    create & modified Experience

---
+++ client/src/Experience.js
@@ -0,0 +1,20 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import CustomCard from './render_components/Card'
+
+const Experience = () => {
+    const experience = [1,2,3,4,5]
+    return ( 
+        <Container className='w-95'>
+            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt distinctio excepturi nulla quod, atque cumque quo tempore corporis veritatis consectetur hic rerum? Reiciendis vero dolorum nulla molestias non, debitis qui iusto quibusdam rem id ratione ipsum, exercitationem omnis odit repellat. Similique totam aperiam exercitationem ut laudantium accusamus minima soluta commodi.</div>
+            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt distinctio excepturi nulla quod, atque cumque quo tempore corporis veritatis consectetur hic rerum? Reiciendis vero dolorum nulla molestias non, debitis qui iusto quibusdam rem id ratione ipsum, exercitationem omnis odit repellat. Similique totam aperiam exercitationem ut laudantium accusamus minima soluta commodi.</div>
+            <Row md={1}  lg={3}>
+                    {experience && experience.map(() => (<CustomCard />))}
+            </Row>
+        </Container>
+    );
+}
+ 
+export default Experience;
