896a6e804f4fba2913c11b46ecaf5156115b607e
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-08 17:27:22 -0800

    modified Technologies

---
+++ client/src/Technologies.js
@@ -0,0 +1,19 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import CustomCard from './render_components/Card';
+import RoundCard from './render_components/RoundCard';
+const Technologies = () => {
+    const experience = [1,2,3,4,5]
+    return ( 
+        <Container className='w-95'>
+            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt distinctio excepturi nulla quod, atque cumque quo tempore corporis veritatis consectetur hic rerum? Reiciendis vero dolorum nulla molestias non, debitis qui iusto quibusdam rem id ratione ipsum, exercitationem omnis odit repellat. Similique totam aperiam exercitationem ut laudantium accusamus minima soluta commodi.</div>
+            <Row md={2}  lg={4}>
+                {experience && experience.map(() => (<RoundCard />))}
+            </Row>
+        </Container>
+    );
+}
+ 
+export default Technologies;
