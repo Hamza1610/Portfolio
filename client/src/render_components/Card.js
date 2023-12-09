369fbf2e5fa68e06c7cc9d2ec3f373ce26fe5cc5
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-09 06:40:00 -0800

    remove listGroup

--- client/src/render_components/Card.js
+++ client/src/render_components/Card.js
@@ -5,18 +5,14 @@ import Col from 'react-bootstrap/Col';
 const CustomCard = ()=>  {
   return (
     <Col>
-      <Card style={{ width: '15rem', height: '20rem' }} className='m-3'>
+      <Card style={{ width: '15rem', height: '15rem' }} className='m-3 bg-secondary' border='primary'>
         <Card.Body>
-          <Card.Title>Project Title</Card.Title>
+          <Card.Title style={{color:'white'}}><b>Project Title</b></Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
         </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>Type: Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Technologies: Dapibus ac facilisis in</ListGroup.Item>
-        </ListGroup>
         <Card.Body>
           <Card.Link href="#">Project Link</Card.Link>
           <Card.Link href="#">Preview Link</Card.Link>
