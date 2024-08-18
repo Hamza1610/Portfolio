cd94c5669e832f3d4e329911c53a272b396da3c1
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-18 15:54:22 -0700

    modify imports

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import CustomCard from './render_components/Card';
+import CustomCard from './components/Card';
 
 const Projects = (props) => {
     const projects = [1,2,3,4,5,6,7,8,9]
