b9ccbff4f0eb7d97e57bbb0a2cc5b9c0671a1d30
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-18 15:02:28 -0700

    delete index, Readme file

--- server/README.md
+++
@@ -1,3 +0,0 @@
-Server side application
-# Created by Hamza Muhammad
-# A website made for Professional portfolio.
--- server/index.js
+++
@@ -1,5 +0,0 @@
-const mysql = require('mysql2');
-const Sequelize = require('sequelize')
-
-console.log(mysql);
-console.log(Sequelize);
