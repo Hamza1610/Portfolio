9a3506ba082ea141128f6630f82d4d74176bf4be
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-19 14:41:15 -0700

    import profileRouter, apiRouter routes

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var profilesRouter = require('./routes/profiles');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -20,8 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', apiRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
