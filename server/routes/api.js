ca70aae88ad56d52fc5441d6f6a95a83c45fa7a7
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-19 14:41:54 -0700

    add test api

---
+++ server/routes/api.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* 
+    API ROUTERS
+*/
+
+/* GET profiles listing. */
+router.get('/profile/get-profiles', function(req, res, next) {
+    res.status(200).json([
+        { path: 'full_stack_developer', json: {} },
+        { path: 'backend_developer', json: {} },
+        { path: 'frontend_developer', json: {} },
+        { path: 'machine_learning_developer', json: {} },
+    ])
+});
+
+module.exports = router;
