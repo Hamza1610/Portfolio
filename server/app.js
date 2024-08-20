bc68714df234a94a19a126d300074811516420db
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-20 11:16:58 -0700

    add authentication router

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var profilesRouter = require('./routes/profiles');
 var apiRouter = require('./routes/api');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
