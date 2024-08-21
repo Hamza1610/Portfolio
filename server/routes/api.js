a133268daedd8936acf3c7c0f80cb1580f99f6f5
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-21 04:58:12 -0700

    modify resu;t array

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,12 +8,12 @@ var router = express.Router();
 
 /* GET profiles listing. */
 router.get('/profile/get-profiles', function(req, res, next) {
-    res.status(200).json([
+    res.status(200).json({result: [
         { path: 'full_stack_developer', json: {} },
         { path: 'backend_developer', json: {} },
         { path: 'frontend_developer', json: {} },
         { path: 'machine_learning_developer', json: {} },
-    ])
+    ]})
 });
 
 module.exports = router;
