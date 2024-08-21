9ad55406cf75e0d8dd1eecd3a625c4ec2a4b8054
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-21 04:56:59 -0700

    remove unused import

--- client/src/Contact_me.js
+++ client/src/Contact_me.js
@@ -1,7 +1,4 @@
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 const ContactMe = () => {
@@ -15,7 +12,7 @@ const ContactMe = () => {
                 <a href='#'><i className='fab fa-twitter m-2 h3'></i></a>
                 <a href="tel:+2349026880099"><i className='fab fa-whatsapp m-2 h3'></i></a>
             </div>
-            <b as='b'>or</b>
+            <b >or</b>
             <Form>
                 <Form.Group style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
                     <Form.Control className='w-100 m-3' size="lg" type="text" placeholder="message me" required/>
