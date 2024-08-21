7cc1e4216073197205d6852591d5a133e4810158
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-21 04:56:40 -0700

    remove unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,34 @@
 import './App.css';
 import './resources/fontawesome-free-6.4.2-web/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import IntroMe from './IntroMe';
-import ContactMe from './Contact_me';
-import Footer from './Footer';
-import Navigation from './Header';
-import CustomAccodion from './components/CustomAccodion';
 import Profile from './ProfilePage';
 import { getProfiles } from './utils/profiles';
-
-// import { Router, Routes, Route} from 'react-router-dom';
-// import { fetchProfiles } from 'utils/profiles';
+import { Router, Routes, Route} from 'react-router-dom';
 
 function App() {
-  // const profiles = await getProfiles();
-
+  const profiles = getProfiles();
+  console.log(profiles);
+  
   return (
-    /** <Router>
+    <Router>
           <div className="App bg-dark">
 
             <Routes>
               { profiles ? profiles.map((profile) => (
                 // how can i pass the profile into the Profile component
                 <Route path={ profile.path } component={Profile} />
-              )) : }
+              )) : ( <Route path='/Profile' component={Profile} /> ) }
             </Routes>
           </div> 
         </Router> 
-    **/
-    <div className="App bg-dark">
-      <Navigation />
-      <IntroMe />
-      <CustomAccodion />
-      <ContactMe />
-      <Footer />
-    </div>
+  
+    // <div className="App bg-dark">
+    //   <Navigation />
+    //   <IntroMe />
+    //   <CustomAccodion />
+    //   <ContactMe />
+    //   <Footer />
+    // </div>
   );
 }
 
