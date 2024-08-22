4edfbc814ee4d65fccdc89bc6d90d0dd6b3fb540
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-22 14:56:21 -0700

    add semicolon to statement

--- server/routes/api.js
+++ server/routes/api.js
@@ -13,7 +13,7 @@ router.get('/profile/get-profiles', function(req, res, next) {
         { path: 'backend_developer', json: {} },
         { path: 'frontend_developer', json: {} },
         { path: 'machine_learning_developer', json: {} },
-    ]})
+    ]});
 });
 
 module.exports = router;
