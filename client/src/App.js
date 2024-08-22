d03b0ad96c6c50ac782969038fc6c565851ea6f7
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-22 15:26:31 -0700

    add / to path method

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,30 @@ import './resources/fontawesome-free-6.4.2-web/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './ProfilePage';
 import { getProfiles } from './utils/profiles';
-import { Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   const profiles = [
-    { path: 'full_stack_developer', json: {} },
-    { path: 'backend_developer', json: {} },
-    { path: 'frontend_developer', json: {} },
-    { path: 'machine_learning_developer', json: {} },
+    { path: '/full_stack_developer', json: {} },
+    { path: '/backend_developer', json: {} },
+    { path: '/frontend_developer', json: {} },
+    { path: '/machine_learning_developer', json: {} },
 ];
   console.log(profiles);
   
   return (
-    <Router>
+    <BrowserRouter>
           <div className="App bg-dark">
 
             <Routes>
-              { profiles ? profiles.map((profile) => (
+              {/* { profiles ? profiles.map((profile) => 
                 // how can i pass the profile into the Profile component
-                <Route path={ profile.path } component={Profile} />
-              )) : ( <Route path='/Profile' component={Profile} /> ) }
+                <Route path={ profile.path } component={ Profile } />
+              ) : <Route path='/Profile' component={ Profile } /> } */}
+              <Route path='/'> <Profile /> </Route>
             </Routes>
           </div> 
-        </Router> 
+    </BrowserRouter> 
   
     // <div className="App bg-dark">
     //   <Navigation />
