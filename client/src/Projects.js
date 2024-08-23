0adbf63464630c36a9c82881e978510d3abdd74b
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-23 14:39:05 -0700

    modify header

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -6,8 +6,9 @@ const Projects = (props) => {
     const projects = [1,2,3,4,5,6,7,8,9]
     return (
         <Container>
+            <h4>Projects</h4>
             <Row md={1}  lg={3}>
-                <h4>Projects</h4>
+                
                 {/* For now project will be rendered as template later from database */}
                 {projects && projects.map((item) => (<CustomCard content = {item}/>))}
             </Row>
