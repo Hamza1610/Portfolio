4bd3cf7adc7f87bbc97bcc753c548872a5858590
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-23 14:38:21 -0700

    modify route mapping

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './ProfilePage';
 import { getProfiles } from './utils/profiles';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import NotFoundPage from './404';
 
 function App() {
   const profiles = [
+    { path: '/', json: {} },
     { path: '/full_stack_developer', json: {} },
     { path: '/backend_developer', json: {} },
-    { path: '/frontend_developer', json: {} },
     { path: '/machine_learning_developer', json: {} },
 ];
-  console.log(profiles);
+  console.log();
   
   return (
     <BrowserRouter>
           <div className="App bg-dark">
 
             <Routes>
-              {/* { profiles ? profiles.map((profile) => 
+              { profiles ? profiles.map((profile, key) => 
                 // how can i pass the profile into the Profile component
-                <Route path={ profile.path } component={ Profile } />
-              ) : <Route path='/Profile' component={ Profile } /> } */}
-              <Route path='/'> <Profile /> </Route>
+                (<Route path={ profile.path } Component={ Profile } key={key} exact/>)
+              ) : (<Route path='/' Component={ Profile } />) }
+
+            <Route path='/NotFound' Component={ NotFoundPage } />
             </Routes>
           </div> 
     </BrowserRouter> 
-  
-    // <div className="App bg-dark">
-    //   <Navigation />
-    //   <IntroMe />
-    //   <CustomAccodion />
-    //   <ContactMe />
-    //   <Footer />
-    // </div>
   );
 }
 
