54c7cb09aaa701ad2dacb24aed2e8c9fa5630c01
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-23 14:37:51 -0700

    add notFound page

---
+++ client/src/404.jsx
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+
+
+
+const NotFoundPage = () => {
+    return (
+        <div className="not-found-page">
+            <h1>404</h1>
+            <p>Page not found</p>
+            <Button  >Go back home</Button>
+        </div>
+    );
+}
+
+export default NotFoundPage;
