a9b4343930c6f05c4a23d96b4ce61ad6ff5166f0
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-23 14:41:12 -0700

    add endpoint to logi and change-credentials

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* POST Log in admin */
+/* GET Log in admin */
 router.get('/log-in', function(req, res, next) {
   res.render('login', { title: 'Login', error: '' });
+});
+/* POST  Login */
+router.post('/login', function(req, res, next) {
+  res.status(200).redirect('/');
+});
+
+
+/* GET Log in admin */
+router.get('/change-credentials', function(req, res, next) {
+  res.render('change_credentials', { title: 'Change credentials', error: '' });
+});
+
+/* UPDATE  Login details */
+router.post('/change-credentials', function(req, res, next) {
+  res.status(200).redirect('/');
 });
 
 module.exports = router;
