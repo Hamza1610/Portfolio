f82bcba39f28c6c9a7143492f09163d61d0819d5
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-24 15:47:29 -0700

    create message Model

---
+++ server/models/message.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const MessageSchema = mongoose.Schema({
+    "Name": {
+        type: String,
+        required: true
+    },
+    "Message": {
+        type: String,
+        required: true
+    },
+ 
+}, { timestamps: true })
+
+const Message = mongoose.model('Projects', MessageSchema);
+
+module.exports = Message;
