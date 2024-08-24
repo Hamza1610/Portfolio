3b9b41da10666798b5f30d45a7ed020a71370a77
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-24 13:38:32 -0700

    add endpoint for testing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const ProfileModel = require('../models/profiles');
+router.get('/add-profile', function(req, res, next) {
+
+
+  
+  res.status(200).json({});
+});
+
 
 /* GET Log in admin */
 router.get('/log-in', function(req, res, next) {
@@ -7,6 +15,20 @@ router.get('/log-in', function(req, res, next) {
 });
 /* POST  Login */
 router.post('/login', function(req, res, next) {
+  const profile = new ProfileModel({
+    path: '/',
+    detail: "Software engineer with specialization in backend"
+})
+
+profile.save()
+.then(() => {
+
+})
+.catch(() => {
+  
+})
+
+console.log(profile);
   res.status(200).redirect('/');
 });
 
