dbf6fdaab8c315fa265595bbe4c0278761ae9357
Hamza1610 <muhammadhamza162003@gmail.com>
2024-08-24 15:49:18 -0700

    import model for opreation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const ProfileModel = require('../models/profiles');
+const ProfileModel = require('../models/profile');
 router.get('/add-profile', function(req, res, next) {
 
 
@@ -18,17 +18,19 @@ router.post('/login', function(req, res, next) {
   const profile = new ProfileModel({
     path: '/',
     detail: "Software engineer with specialization in backend"
-})
+  })
 
-profile.save()
-.then(() => {
-
-})
-.catch(() => {
+  profile.save()
+  .then((result) => {
+    console.log('Result from saving profile:', result);
   
-})
+  })
+  .catch((error) => {
+    console.log(error);
+    res.status(200).redirect('/', { error: 'Profile not saved' });
+  });
 
-console.log(profile);
+  console.log(profile);
   res.status(200).redirect('/');
 });
 
