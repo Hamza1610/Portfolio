747501aee22839f7bd938df57f3a6f4189cf8217
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-06 13:33:31 -0700

    add static directory for bootstrap

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.min.js')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
