e2cf0f6c9b56030f10ea49f1b0b1bec36921dc61
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-07 00:35:37 -0700

    add ejs-layout features

--- server/app.js
+++ server/app.js
@@ -3,27 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var profilesRouter = require('./routes/profiles');
 var apiRouter = require('./routes/api/profile');
 var authRouter = require('./routes/auth');
+var expressEjsLayouts = require('express-ejs-layouts');
 
 var app = express();
 
 // view engine setup
 
-app.set('views', path.join(__dirname, 'views/pages'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set(expressEjsLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.min.js')));
-
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { layout: 'layouts/aut' });
 });
 
 module.exports = app;
