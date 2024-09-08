7e97b606f0fd03b3994fb6b44f973eba3ccce9ef
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-08 15:22:48 -0700

    add locals to render page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+// document head data
+var headLocals = {
+  link: { rel: "stylesheet",  href: "/css/bootstrap.css" },
+  link: { rel: "stylesheet",  href: "/css/bootstrap-theme.css" },
+  script: { type: "application/javascript",  src: "/js/bootstrap.js"}
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { layout: 'layouts/dashboard.layout.ejs' });
+  res.render('layouts/layout', { title: 'Dashboard', body: "<%- include('pages/index') %>" });
 });
+
+router.get('/blog', function(req, res, next) {
+
+  res.render('pages/bootsrap.pages/blog');
+});
+
+router.get('/dashboard', function(req, res, next) {
+
+  res.render('pages/bootsrap.pages/dashboard');
+});
+
+router.get('/album', function(req, res, next) {
+
+  res.render('pages/bootsrap.pages/album');
+});
+
+router.get('/carousel', function(req, res, next) {
+
+  res.render('pages/bootsrap.pages/carousel');
+});
+
+router.get('/mensarry', function(req, res, next) {
+
+  res.render('pages/bootsrap.pages/mensarry');
+});
+
+router.get('/product', function(req, res, next) {
+
+  res.render('pages/bootsrap.pages/product');
+});
+
 
 module.exports = router;
