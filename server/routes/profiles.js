d0bb6ef480698d01e48d338d86599041b65172ca
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-13 16:01:45 -0700

    add profiles endpoint for new profile

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Profile page. */
-router.get('/profiles', function(req, res, next) {
-  res.render('pages/profiles', { title: 'Profiles', body: "<%- include('pages/profiles') %>" });
+/* GET Profiles page. */
+router.get('/', function(req, res, next) {
+  // body: "<%- include('./pages/profiles.ejs') %>" 
+  res.render('pages/profiles', { title: 'Profiles'});
+});
+
+
+/* GET Create Profile page. */
+router.get('/create_profile', function(req, res, next) {
+  // body: "<%- include('./pages/profiles.ejs') %>" 
+  res.render('pages/new_profile', { title: 'New Profile'});
 });
 
 module.exports = router;
