8bd59e74db6a8e873f2d7b8fbf570abcdd8f13b3
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-12 17:18:49 -0700

    modify render page

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,7 +10,7 @@ var headLocals = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('layouts/layout', { title: 'Dashboard', body: "<%- include('pages/index') %>" });
+  res.render('layouts/layout', { title: 'Dashboard', body: "<%- include('pages/partials/nav.ejs') %>" });
 });
 
 router.get('/blog', function(req, res, next) {
