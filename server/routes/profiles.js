d5a901110122f3d7cedfff1e0cda63e6182ba06c
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-16 13:09:59 -0700

    add fetchProfiles service function

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,17 +1,33 @@
+var fetchProfiles = require('../services/Profile');
 var express = require('express');
 var router = express.Router();
 
 /* GET Profiles page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   // body: "<%- include('./pages/profiles.ejs') %>" 
-  res.render('pages/profiles', { title: 'Profiles'});
-});
+  try {
+    const profiles = (await fetchProfiles()).profiles
+    console.log(profiles);
+    res.render('pages/profiles', { title: 'Profiles', profiles }); 
 
+  } catch (error) {
+    console.log(error);
+    res.render('pages/profiles', { title: 'Profiles', error }); 
+  }   
+});
 
 /* GET Create Profile page. */
 router.get('/create_profile', function(req, res, next) {
   // body: "<%- include('./pages/profiles.ejs') %>" 
   res.render('pages/new_profile', { title: 'New Profile'});
 });
+
+/* GET Create Profile page. */
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id
+  
+  res.render('pages/edit_profile', { title: 'Edit Profile'});
+});
+
 
 module.exports = router;
