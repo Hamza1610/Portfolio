b81151c264a08bfd5da681308f6fe1181e13c4f6
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-16 22:04:48 -0700

    create element selector

---
+++ server/public/javascripts/new_profile.js
@@ -0,0 +1,43 @@
+
+    // get all element by their id
+    const name = document.getElementById('fullName');
+    const title = document.getElementById('title');
+    const address = document.getElementById('address');
+
+    // Education
+    let institution = document.getElementById('institution');
+    let eduStarted = document.getElementById('edu_started');
+    let eduFinish = document.getElementById('edu_finish');
+
+
+    // Exprerience
+    let roleTitle = document.getElementById('role_title');
+    let roleDescription = document.getElementById('role_description');
+    let roleStarted = document.getElementById('role_started');
+    let roleFinish = document.getElementById('role_finish');
+
+    // Summary
+    let summary = document.getElementById('summary');
+    
+    // Contact
+    let phoneNumber = document.getElementById('phoneNumber');
+    let github = document.getElementById('github');
+    let linkedin = document.getElementById('linkedin');
+    let x = document.getElementById('x');
+
+    // Certifications
+    let certType = document.getElementById('cert_type');
+    let certImage = document.getElementById('cert_image');
+
+    // Programming Languages
+    let framework = document.getElementById('framework');
+
+    // Skills
+    let skills = document.getElementById('skills'); 
+    
+    // Projects
+    let projectTitle = document.getElementById('project_title');    
+    let projectSummary = document.getElementById('project_summary');    
+    let projectTechnologies = document.getElementById('project_technologies');    
+    let projectGithubLink = document.getElementById('project_github_link');    
+    let projectLiveLink = document.getElementById('project_live_link');    
