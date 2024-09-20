33a42aa5eb3436d9b2824eacd7c465871a96cc0f
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-19 19:18:10 -0700

    add comma

--- server/models/profile.js
+++ server/models/profile.js
@@ -5,7 +5,7 @@ const ProfileSchema = mongoose.Schema({
     Title: {
         type: String,
         required: true
-    }
+    },
 }, { timestamps: true })
 
 const Profile = mongoose.model('Profile', ProfileSchema);
