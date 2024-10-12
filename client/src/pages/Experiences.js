8cbd3c8866965532eedce100b92b997b788c14fc
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-12 15:36:58 -0700

    modify to framer-motion div styles

--- client/src/pages/Experiences.js
+++ client/src/pages/Experiences.js
@@ -1,24 +1,31 @@
 import Container from 'react-bootstrap/Container';
+import { motion } from "framer-motion";
 
 
 const Experiences = ({ experience }) => {
     
     console.log(experience)
-    return ( 
-        <Container id='experience' className='w-95 mb-3' >
-            <h4>Experience</h4>
-            <hr style={{ width: '50vw',  height:'1em' }}/>
-            <ul>
-                {experience && experience.map((item, key) => (
-                    <li key={key}>
-                        <div>
-                            <h4>{item.Title}</h4>
-                            <p>{item.Description}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </Container>
+    return (
+        <motion.div
+            // whileInView={{ opacity: 1, x: 0 }}
+            // initial={{ opacity: 0.2, x: 5 }}
+            // transition={{ duration: .75, delay: .5 }}
+    >
+            <Container id='experience' className='w-95 mb-3' >
+                <h4>Experience</h4>
+                <hr style={{ width: '50vw',  height:'1em' }}/>
+                <ul>
+                    {experience && experience.map((item, key) => (
+                        <li key={key}>
+                            <div>
+                                <h4>{item.Title}</h4>
+                                <p>{item.Description}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </Container>
+        </motion.div> 
     );
 }
  
