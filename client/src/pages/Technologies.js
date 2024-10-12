e2d6dcf4aacafc03f54eaa5641d363682a949226
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-12 15:38:20 -0700

    modify to framer-motion div styles

--- client/src/pages/Technologies.js
+++ client/src/pages/Technologies.js
@@ -1,13 +1,16 @@
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import RoundCard from '../components/RoundCard';
+import { motion } from "framer-motion";
 
 const Technologies = ({ technologies }) => {
-    return ( 
+    return (       
         <Container id='technologies' className='w-95 mb-5'>
-            <h4>Technologies</h4>
-            <hr style={{ width: '50vw',  height:'1em' }}/>
-            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+            <motion.h4
+                
+                >Technologies</motion.h4>
+                <hr style={{ width: '50vw',  height:'1em' }}/>
+            <motion.div>I'm exerienced in working with the following programming languages, frameworks and services</motion.div>
             <Row md={2}  lg={4}>
                 {technologies && technologies.map((item, key) => (<RoundCard key={key} imageSource={item.Framework} />))}
             </Row>
