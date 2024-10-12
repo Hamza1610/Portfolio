f7299d0867e0fd15a0ab7f1e97c52c5eae44807c
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-12 15:35:40 -0700

    modify style to framer-motion

--- client/src/components/RoundCard.js
+++ client/src/components/RoundCard.js
@@ -1,13 +1,16 @@
-
 import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
-import logo from '../logo.svg';
+import { motion } from 'framer-motion';
+import '../App.css';
+
 const RoundCard = ({ imageSource }) => {
     return (
-        <Card style={{width:'100px', height:'100px',}}  className='m-2 bg-dark d-flex justify-content-center align-items-center' border='primary'>
-            <Image src={imageSource} alt='Technology Image' style={{width:'90px', height:'90px'}} roundedCircle/>
-            {/* <Image src={logo} alt='Technology Image' style={{width:'90px', height:'90px'}} roundedCircle/> */}
-        </Card>
+        <motion.div
+        
+            style={{width:'100px', height:'100px'}}
+            className='m-2 bg-dark d-flex justify-content-center align-items-center'
+        >
+            <Image className="round-card-image" src={imageSource} alt='Technology' roundedCircle/>
+        </motion.div>
     )
 }
 export default RoundCard;
