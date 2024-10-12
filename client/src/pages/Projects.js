aab376a49927edbc1d5f1390c44fa65407fd4152
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-12 15:37:53 -0700

    modify to framer-motion div styles

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,15 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import ProjectCard from '../components/ProjectCard';
+import { motion } from "framer-motion";
 
 const Projects = ({ projectsList }) => {
 
     console.log(projectsList);
     return (
         <Container id='projects' className='mt-4 mb-3'>
-            <h4>Projects</h4>
+            <motion.h4
+            
+            
+            >Projects</motion.h4>
             <hr style={{ width: '50vw',  height:'1em' }}/>
             <div className="d-flex justify-content-center align-items-center flex-wrap gap-3">
-                
+                    
                 {/* For now project will be rendered as template later from database */}
                 {projectsList && projectsList.map((item, key) => (<ProjectCard key={key} projectContent = {item}/>))}
             </div>
