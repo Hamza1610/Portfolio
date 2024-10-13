4dc1788c8ceb9a932d8597e3dba4b4f837c9a1f6
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-12 19:02:34 -0700

    add framer style

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -8,8 +8,10 @@ const Projects = ({ projectsList }) => {
     return (
         <Container id='projects' className='mt-4 mb-3'>
             <motion.h4
-            
-            
+                whileInView={{ opacity: 1, x: 0}}
+                initial={{ opacity: 0.2, x: 25 }}
+                transition={{ type: 'spring', damping: 10, stiffness: 100,  duration: 1.4, delay: .75 }}         
+
             >Projects</motion.h4>
             <hr style={{ width: '50vw',  height:'1em' }}/>
             <div className="d-flex justify-content-center align-items-center flex-wrap gap-3">
