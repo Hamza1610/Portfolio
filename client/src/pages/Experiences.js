456d31a5e4178692c2374fbf768773696820efe9
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-12 19:01:05 -0700

    add framer style

--- client/src/pages/Experiences.js
+++ client/src/pages/Experiences.js
@@ -12,15 +12,24 @@ const Experiences = ({ experience }) => {
             // transition={{ duration: .75, delay: .5 }}
     >
             <Container id='experience' className='w-95 mb-3' >
-                <h4>Experience</h4>
+                <motion.h4
+                    whileInView={{ opacity: 1, x: 0}}
+                    initial={{ opacity: 0.2, x: 20 }}
+                    transition={{ type: 'spring', damping: 10, stiffness: 100,  duration: 1.4, delay: .75 }} 
+                >Experience</motion.h4>
                 <hr style={{ width: '50vw',  height:'1em' }}/>
                 <ul>
                     {experience && experience.map((item, key) => (
                         <li key={key}>
-                            <div>
-                                <h4>{item.Title}</h4>
-                                <p>{item.Description}</p>
-                            </div>
+                            <motion.div
+                                whileInView={{ opacity: 1, scale: 1}}
+                                initial={{ opacity: 0.2, scale: .2 }}
+                                transition={{ type: 'spring', damping: 10, stiffness: 100,  duration: 1.4, delay: .75 }} 
+                            
+                            >
+                                <h6>{item.Title}</h6>
+                                <p style={{fontSize: '.9rem'}}>{item.Description}</p>
+                            </motion.div>
                         </li>
                     ))}
                 </ul>
