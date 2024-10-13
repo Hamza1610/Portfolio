5eb657a38b47c4f33f41c3f62b1defa519f4be4e
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-12 18:59:53 -0700

    add framer style

--- client/src/pages/Technologies.js
+++ client/src/pages/Technologies.js
@@ -7,7 +7,9 @@ const Technologies = ({ technologies }) => {
     return (       
         <Container id='technologies' className='w-95 mb-5'>
             <motion.h4
-                
+                    whileInView={{ opacity: 1, x: 0}}
+                    initial={{ opacity: 0.2, x: 20 }}
+                    transition={{ type: 'spring', damping: 10, stiffness: 100,  duration: 1.4, delay: .75 }} 
                 >Technologies</motion.h4>
                 <hr style={{ width: '50vw',  height:'1em' }}/>
             <motion.div>I'm exerienced in working with the following programming languages, frameworks and services</motion.div>
