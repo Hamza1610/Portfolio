2156b02adf5d6b4ec2417cbc7a7c526e634051dd
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-03 18:34:07 -0800

    modify asyncronous data

--- client/src/ProfileList.jsx
+++ client/src/ProfileList.jsx
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react'
 import { motion } from "framer-motion";
 import axios from 'axios'
 import ProfileCard from './components/ProfileCard'
+import localProfilesList from './localProfilesTitleTable.json'
+
 
 const  ProfileList = () => {
 
-    const [profiles, setProfiles] = useState([])
+    const [serverProfiles, setServerProfiles] = useState([])
+    const [localProfiles, setLocatProfiles] = useState(localProfilesList)
     const [error, setError] = useState('')
 
     useEffect(() => {
+        console.log('Local Porfiles:', localProfiles);
+        
         const fetchProfileTitles = async () => {
             try {
                 const response = await axios.get('http://localhost:3020/api/get-all-profiles');
@@ -16,8 +21,8 @@ const  ProfileList = () => {
                 console.log('Profiles', response);
                 
                 if (response.status === 200) {
-                    setProfiles(response.data.profiles)
-                    console.log('Profile list:', profiles);
+                    setServerProfiles(response.data.profiles)
+                    console.log('Profile list:', serverProfiles);
                 } else {
                     setError('Error fetcing profile')
                 }
@@ -33,28 +38,61 @@ const  ProfileList = () => {
 
     return (
         <div
-            style={{width: '100vw',
-                display: 'flex',
-                justifyContent:'center',
-                alignItems: 'center',
-                height: '100vh'
-                }}>
-            <div style={{width: '80%',
-                display: 'flex',
-                justifyContent:'space-evenly',
-                alignItems: 'center',
-                flexWrap:'wrap', gap: '20px'}}>
-                {profiles.length < 1 && (
+            style={{
+                width: '100vw',
+                display: 'block',
+            }}>
+            {error && ('ERROR: ' + error)}
+            <div>
+                {serverProfiles ? (
+                    <div
+                        style={{width: '70%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            flexWrap: 'wrap',
+                            justifyContent:'space-evenly',
+                            alignItems: 'center',
+                            gap: '20px'}}
+                    >
+                        <h3>Server Profiles</h3>
+                        <hr style={{ width: '50%',  height:'1em' }}/>
+                        {serverProfiles.map((profile, index) => (<ProfileCard key={index} {...profile }/>))}
+                    </div>
+                ) : (
                     <motion.div
                         whileInView={{ opacity: 1, x: 0 }}
                         initial={{ opacity: 0.2, x: 70 }}
                         transition={{ duration: .75, delay: .5 }}
                     >
                         <h3>No Available Profile</h3>
-                        <p style={{ textAlign: 'center' }}>Please refresh your browser</p>
+                        <p style={{ textAlign: 'center' }}>No Server Profile Available</p>
                     </motion.div>
                 )}
-                {profiles && profiles.map((profile, index) => (<ProfileCard key={index} {...profile }/>))}
+
+            </div>
+
+            <div>
+                {localProfiles ? (
+                    <div style={{
+                        width: '70%',
+                        alignItems: 'center',
+                        gap: '20px'
+                        }}
+                    >
+                        <h3>Local Profiles</h3>
+                        <hr style={{ width: '50%',  height:'1em' }}/>
+                        {localProfiles.map((profile, index) => (<ProfileCard key={index} {...profile } />))}
+                        </div>
+                    ) : (
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0.2, x: 70 }}
+                            transition={{ duration: .75, delay: .5 }}
+                        >
+                            <h3>No Available Profile</h3>
+                            <p style={{ textAlign: 'center' }}>No Local Profile Available</p>
+                        </motion.div>
+                    )}
             </div>
         </div>
       );
