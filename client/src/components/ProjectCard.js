b34e0b41b9e61dae9da24d8ee593fed7ae7220ea
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-04 07:16:19 -0800

    add heihgt to card

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -7,7 +7,7 @@ const ProjectCard = ({ projectContent })=>  {
       className='m-0 p-0 d-flex justify-content-center align-items-center'
       whileHover={{scale: 1.1}}
       >
-      <Card style={{ width: '12rem', height: '18rem', fontSize: '.5rem'}} className='custon-card m-0 bg-secondary fs-5' border='primary'>
+      <Card style={{ width: '12rem', height: '25rem', fontSize: '.5rem'}} className='custon-card m-0 bg-secondary fs-5' border='primary'>
         <Card.Body>
           <Card.Title style={{color:'white'}}><h5>{projectContent.Title || 'Project Title'}</h5></Card.Title>
           <Card.Text>
