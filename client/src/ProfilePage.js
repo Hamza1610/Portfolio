c4aaa95b6037613dbf9d5f91ddefc0cbe9b58c43
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-03 18:34:23 -0800

    modify asyncronous data

--- client/src/ProfilePage.js
+++ client/src/ProfilePage.js
@@ -8,42 +8,81 @@ import Footer from './Footer';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import localProfilesList from './localProfilesTable.json'
 
 const Profile = () => {
   const { id: profileId } = useParams(); // Destructure to get 'id' from useParams
-  console.log('userParams > profileId', profileId);
   const [profile, setProfile] = useState(null); // Initially null, assuming profile is an object
+  const [profileIdState, setProfileIdState] = useState(null); // 
   const [error, setError] = useState(''); // Empty string as the default error state
   const [loading, setLoading] = useState(true); // Add loading state
 
-  useEffect(() => {
-    const fetchProfileData = async () => {
-      try {
-        console.log('Received id:', profileId);
-        const response = await axios.get(`http://localhost:3020/api/get-profile/${profileId}`);
+  console.log('ProfileId from params:', profileId);
+  
+  function fetchLocalProfile(profileId) {
 
-        if (response.status === 200) {
-          setProfile(response.data); // Set the profile data on success
-          console.log(profile); // Set the profile data on success
-        } else {
-          setError('No Profile data found');
+    let localProfile;
+
+    for (let profile of localProfilesList) {
+      console.log('Iterated: ', profile)
+      console.log(profile._id, profileId)
+      if (profile._id === profileId) {
+        console.log('Found profile >', profile);
+        localProfile = profile
+        break
+      };
+    }
+
+    if (!localProfile) return null;
+    return localProfile
+  }
+
+  const fetchServerProfile = async (profileId) => {
+    const response = await axios.get(`http://localhost:3020/api/get-profile/${profileId}`);
+
+    if (response.status === 200) {
+
+      const  data = response.data;
+      return data; 
+    } else {
+      return null;
+    } 
+  }
+
+  useEffect(() => {
+    const getProfileFromSource = async () => {
+      try {
+        if (profileId.length <= 5) {
+          console.log('-----------------Local----------------------')
+          setProfileIdState(profileId);
+          console.log('ProfileId check: ', profileId);
+          const profileData = await fetchLocalProfile(Number(profileId))
+          setProfile(profileData)
+        }
+        else {
+          console.log('-----------------Server----------------------')
+          setProfileIdState(profileId)
+          console.log('ProfileId check: ', profileIdState);
+          const profileData = await fetchServerProfile(profileId)
+          setProfile(profileData)
         }
       } catch (error) {
         console.error('Error fetching profile:', error);
-        setError('Failed to fetch profile data'); // Set user-friendly error message
+        setError('Failed to fetch profile data');
       } finally {
-        setLoading(false); // Stop loading after the fetch attempt
+        setLoading(false);
       }
-    };
-
-    fetchProfileData(); // Fetch the profile data when the component mounts
+    }
+    getProfileFromSource()
   }, [profileId]); // The effect depends on profileId
 
   // Display loading or error if necessary
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  if (loading) return <div syle={{height: '100vh'}}>Loading...</div>;
+  if (error) return <div syle={{height: '100vh'}}>Error: {error}</div>;
 
-  const { PersonalInfo, Education, Experience, Contact, Skill, Summary, Framework, Project } = profile;
+  console.log('Profile before destructure', profile);
+  
+  const { PersonalInfo, Experience, Contact, Summary, Framework, Project } = profile;
 
   return (
     <div id="app" className="App bg-dark">
