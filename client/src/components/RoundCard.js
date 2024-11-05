39373db01c2953ee9a11ee18bc87c7a9dd0190bd
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-05 03:28:25 -0800

    modify view for framework Label

--- client/src/components/RoundCard.js
+++ client/src/components/RoundCard.js
@@ -2,14 +2,17 @@ import Image from 'react-bootstrap/Image';
 import { motion } from 'framer-motion';
 import '../App.css';
 
-const RoundCard = ({ imageSource }) => {
+const RoundCard = ({ imageSource, Label }) => {
+    if (Label) console.log(Label);
+    
     return (
         <motion.div
         
             style={{width:'100px', height:'100px'}}
-            className='m-2 bg-dark d-flex justify-content-center align-items-center'
+            className='m-2 bg-dark d-flex justify-content-center align-items-center flex-column m-4'
         >
             <Image className="round-card-image" src={imageSource} alt='Technology' roundedCircle/>
+            <b>{Label}</b>
         </motion.div>
     )
 }
