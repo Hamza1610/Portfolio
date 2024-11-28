758cefb1ced5a03332019ea42374310f3c232fb4
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-28 12:07:20 -0800

    add moveToTop function to scroller event

--- client/src/components/TopScroller.js
+++ client/src/components/TopScroller.js
@@ -2,8 +2,14 @@ import '../App.css';
 
 const TopScroller = () => {
 
+    const moveToTop = (event) => {
+        setTimeout(() =>{
+            event.preventDefault();
+        }, 1500)
+      }
+
     return ( 
-        <a href="#app" style={{display: 'absolute'}}>
+        <a href="#app" onClick={(event) => moveToTop(event)} style={{display: 'absolute'}}>
             <div className="bg-primary top-scroller">
                 <i className="fas fa-up-long  arrow"></i>            
             </div>
