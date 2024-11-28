e88b5cd91433a39bb1962390f19fd2f4748fb6b4
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-28 12:06:34 -0800

    modify profile card

--- client/src/components/ProfileCard.jsx
+++ client/src/components/ProfileCard.jsx
@@ -13,8 +13,8 @@ const ProfileCard = ({ ...profile }) => {
                 <Image alt='Profile' src='my profile picture.jpg' style={{width: '100%'}}/>
                 <Card.Title style={{color:'white'}}><b>{ profile.Title }</b></Card.Title>
                 <div style={{ width: '100%', height:'30px', display: 'flex', gap: '5px'}}>
-                    <Link onClick={() => navigator.clipboard.writeText( baseUrl + 'profile/' + profile._id)}  class='btn btn-primary' style={{ width: '50%',  height: 'auto', fontSize: '.7rem', color: 'white' }} >Copy</Link>
-                    <Link class='btn btn-primary' to={'/profile/' + profile._id}  style={{ width: '50%', height: 'auto', fontSize: '.7rem', color: 'white'}} >Open </Link>
+                    <Link onClick={() => navigator.clipboard.writeText( baseUrl + 'profile/' + profile._id)}  class='btn btn-primary d-flex justify-content-center align-items-center' style={{ width: '50%',  height: 'auto', fontSize: '.7rem', color: 'white' }} >Copy</Link>
+                    <Link class='btn btn-primary d-flex justify-content-center align-items-center' to={'/profile/' + profile._id}  style={{ width: '50%', height: 'auto', fontSize: '.7rem', color: 'white'}} >Open </Link>
                 </div>
             </Card.Body>
         </Card>
